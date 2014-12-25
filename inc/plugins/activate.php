02993b4ef5450864376e0504ac82781bc3ff0e56
PaulBender <paulbender@live.com>
2014-12-25 13:33:59 -0500

    Added codename

--- inc/plugins/activate.php
+++ inc/plugins/activate.php
@@ -41,6 +41,7 @@ function activate_info()
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
 		"version"			=> "1.0",
+		"codename"			=> "activate",
 		"compatibility"		=> "18*"
 	);
 }
